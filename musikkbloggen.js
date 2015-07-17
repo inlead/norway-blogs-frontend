9a312590a82b31b289e8437ba544f44661568e8c
id <id@util.inlead.dk>
2015-07-17 15:47:16 +0200

    EMUSIKNO-69 - Added code docs to avoid mix up between blogs.

--- musikkbloggen.js
+++ musikkbloggen.js
@@ -1,3 +1,4 @@
+// Additional JS for bergen blog.
 jQuery( document ).ready(function() {
   jQuery('.widget_indexdata_widget .fullwidth').parent().insertBefore(jQuery('.authorarea').prev());
 });
--- musikkprat.js
+++ musikkprat.js
@@ -1,3 +1,4 @@
+// Addtional JS for trondheim blog.
 jQuery( document ).ready(function() {
   jQuery('.widget_indexdata_widget .fullwidth').parent().insertAfter(jQuery('.comments-area').prev());
 });
