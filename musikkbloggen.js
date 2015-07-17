db32f2f4fda028af1e574f0074b6cb63afbae607
id <id@util.inlead.dk>
2015-07-17 15:45:27 +0200

    EMUSIKNO-69 - Added file under git control.

---
+++ musikkbloggen.js
@@ -0,0 +1,4 @@
+jQuery( document ).ready(function() {
+  jQuery('.widget_indexdata_widget .fullwidth').parent().insertBefore(jQuery('.authorarea').prev());
+});
+
---
+++ musikkprat.js
@@ -0,0 +1,4 @@
+jQuery( document ).ready(function() {
+  jQuery('.widget_indexdata_widget .fullwidth').parent().insertAfter(jQuery('.comments-area').prev());
+});
+
